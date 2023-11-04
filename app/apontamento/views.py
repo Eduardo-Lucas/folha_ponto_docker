705268bf39b5c25635204eb38114564c466900a8
Eduardo-Lucas <eduardolucas40@gmail.com>
2023-11-03 23:39:59 -0300

    gitignore updated

--- app/apontamento/views.py
+++ app/apontamento/views.py
@@ -16,34 +16,40 @@ def apontamento_list(request, login_url="users:login"):
     return render(request, "apontamento/apontamento-list.html", {})
 
 
-def folha_ponto(request):        
+def folha_ponto(request):  
+    # get sum  of differences group by date #
+    #       
     usuario = User.objects.filter(username="sara").first()
     service = PontoService()
+
+    data_inicial=pytz.timezone(settings.TIME_ZONE).localize(datetime(2023, 9, 1, 0, 0, 0)).strftime("%Y-%m-%d")
+    data_final  =pytz.timezone(settings.TIME_ZONE).localize(datetime(2023, 9, 3, 0, 0, 0)).strftime("%Y-%m-%d")
+
     pontos = service.ponto_list(
         usuario.id,
-        data_inicial=pytz.timezone(settings.TIME_ZONE).localize(datetime(2023, 9, 1, 0, 0, 0)),
-        data_final  =pytz.timezone(settings.TIME_ZONE).localize(datetime(2023, 9, 22, 0, 0, 0)),
+        data_inicial,
+        data_final,
     )
 
     pontos_sumarizados = []
     horas_trabalhadas = timedelta(0)
+    total_horas_trabalhadas = timedelta(0)  # total hours worked across all days
+    dia = data_inicial
     for ponto in pontos:
-        dia = ponto.entrada.date()
-        if ponto.entrada.date() == dia:
-            horas_trabalhadas += ponto.difference
-            dia = ponto.entrada.date()
-        else:    
-            print("DIA", dia, "HORAS TRABALHADAS", horas_trabalhadas)
-            dict_ponto = {"dia": dia, "horas_trabalhadas": horas_trabalhadas}
-            pontos_sumarizados.append(dict_ponto)
+        if ponto.entrada.date() != dia:
+            pontos_sumarizados.append(
+                {
+                    "dia": dia,
+                    "horas_trabalhadas": horas_trabalhadas,
+                }
+            )
+            total_horas_trabalhadas += horas_trabalhadas  # add the hours worked for the day to the total
             horas_trabalhadas = timedelta(0)
-
-
-    nome = usuario.username
-    context = {"pontos_sumarizados": pontos_sumarizados, 
-               "pontos": pontos, 
-               "nome": nome}
-
+            dia = ponto.entrada.date()
+        horas_trabalhadas += ponto.difference
+    total_horas_trabalhadas += horas_trabalhadas  # add the hours worked for the last day to the total
+    
+    context = {"pontos_sumarizados": pontos_sumarizados, "total_horas_trabalhadas": total_horas_trabalhadas}
 
     return render(
         request,
