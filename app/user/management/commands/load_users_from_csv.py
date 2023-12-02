22b77ff38cdd565b4e45e96919ae9bebe7065510
Eduardo-Lucas <eduardolucas40@gmail.com>
2023-12-02 13:24:57 -0300

    Change for loaddata

--- app/user/management/commands/load_users_from_csv.py
+++ app/user/management/commands/load_users_from_csv.py
@@ -1,24 +1,20 @@
-import csv
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
+from django.core.management import call_command
 
 
 class Command(BaseCommand):
-    help = 'Load users from a CSV file'
+    help = "This command is used to load fixtures"
 
-    def add_arguments(self, parser):
-        parser.add_argument('Usuario.csv', type=str)
+    def handle(self, *args, **kwargs):
+        fixtures = [
+            "Usuario",
+        ]
 
-    def handle(self, *args, **options):
-        with open(options['user/fixtures/Usuario.csv'], newline='', encoding='utf-8') as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                if not User.objects.filter(username=row['username']).exists():
-                    User.objects.create_user(
-                        username=row['username'],
-                        password=row['password'],
-                        email=row['e-mail']
-                    )
-                    print(f'User {row["username"]} has been created.')
-                else:
-                    print(f'User {row["username"]} already exists.')
+        try:
+
+            for fixture in fixtures:
+                self.stdout.write(self.style.MIGRATE_HEADING(fixture + ": "))
+                call_command("loaddata", fixture)
+            self.stdout.write(self.style.SUCCESS("Finished loading fixtures"))
+        except Exception:
+            self.stdout.write(self.style.ERROR("An exception occurred while loading fixtures"))
