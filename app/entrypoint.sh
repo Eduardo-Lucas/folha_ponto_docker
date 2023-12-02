1c78952f9ed1b9ed250a37b247c9c865593fdc7c
Eduardo-Lucas <eduardolucas40@gmail.com>
2023-12-02 12:28:10 -0300

    Amend entrypoint file

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -2,5 +2,6 @@
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsu load_users_from_csv
+python manage.py createsu
+python manage.py load_users_from_csv
 gunicorn folha_ponto.wsgi:application --bind 0.0.0.0:10000
