878ad34797f7b691938f366f584ffb6663ac9445
Eduardo-Lucas <eduardolucas40@gmail.com>
2023-12-02 18:49:58 -0300

    Update entrypoint

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -3,5 +3,4 @@
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py createsu
-python manage.py load_users_from_csv user/fixtures/Usuario.csv
 gunicorn folha_ponto.wsgi:application --bind 0.0.0.0:10000
