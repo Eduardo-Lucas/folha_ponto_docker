79d00fe7efc88e9d4ac358a2d7ea92a4f565348f
Eduardo-Lucas <eduardolucas40@gmail.com>
2024-01-07 17:13:26 -0300

    Turning Contato table off

--- app/contato/admin.py
+++ app/contato/admin.py
@@ -1,17 +1,15 @@
-from django.contrib import admin
-
-from import_export.admin import ImportExportModelAdmin
-from import_export import resources
 from contato.models import Contato
+from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
+# class ContatoResource(resources.ModelResource):
+#     class Meta:
+#         model = Contato
 
 
-class ContatoResource(resources.ModelResource):
-    class Meta:
-        model = Contato
+# class ContatoAdmin(ImportExportModelAdmin):
+#     resource_classes = [ContatoResource]
 
 
-class ContatoAdmin(ImportExportModelAdmin):
-    resource_classes = [ContatoResource]
-
-
-admin.site.register(Contato, ContatoAdmin)
+# admin.site.register(Contato, ContatoAdmin)
