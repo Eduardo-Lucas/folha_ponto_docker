7a93b09d8bcac59a32a97acf88f4dc4be9b9f3e9
Eduardo-Lucas <eduardolucas40@gmail.com>
2024-02-08 13:11:57 -0300

    Less 1 validator

--- app/folha_ponto/settings.py
+++ app/folha_ponto/settings.py
@@ -151,9 +151,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
-    {
-        "NAME": "django.contrib.auth.password_validation.UserChangedPasswordValidator",
-    },
+
 ]
 
 
