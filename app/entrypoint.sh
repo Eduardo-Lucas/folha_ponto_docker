06db5f4534cd835d00de3dc78814a96ece9bfa7c
Eduardo-Lucas <eduardolucas40@gmail.com>
2024-02-12 10:37:03 -0300

    makemigrations & migrate are back

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -3,11 +3,11 @@
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
-# python manage.py createuser # normal user
-# python manage.py createsu # super user
-# python manage.py load_tipo_receita # Tipo Receita
-# python manage.py load_contato # Contatos not needed anymore
-# python manage.py load_feriado # Feriados
-# python manage.py load_clientes # Clientes
+python manage.py createuser # normal user
+python manage.py createsu # super user
+python manage.py load_tipo_receita # Tipo Receita
+python manage.py load_contato # Contatos not needed anymore
+python manage.py load_feriado # Feriados
+python manage.py load_clientes # Clientes
 # python manage.py load_pontos_pandas # Pontos-2023
 gunicorn folha_ponto.wsgi:application --bind 0.0.0.0:10000
