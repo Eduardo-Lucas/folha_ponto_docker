da922028b1751a94c3a12660d606c4142d12f91b
Eduardo-Lucas <eduardolucas40@gmail.com>
2024-02-24 20:24:33 -0300

    house keeping

--- app/folha_ponto/celery.py
+++ app/folha_ponto/celery.py
@@ -5,9 +5,9 @@ import os
 from celery import Celery
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "folha_on_docker.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "folha_ponto.settings")
 
-app = Celery("folha_on_docker")
+app = Celery("folha_ponto")
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
