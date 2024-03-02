b7ddbf08dc7be62efa0ec3e12fa9be84bb710eb5
Eduardo-Lucas <eduardolucas40@gmail.com>
2024-03-01 22:37:28 -0300

    pontos abertos e fechados na home page

--- app/core/views.py
+++ app/core/views.py
@@ -1,18 +1,19 @@
 from datetime import datetime
 
 from apontamento.models import Ponto
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.paginator import Paginator
 from django.shortcuts import render
 
 
-@login_required
 def home(request):
     """Home Page"""
     users = User.objects.filter(is_active=True)
-    pontos = []
+
+    pontos_abertos = []
+    pontos_fechados = []
+
     for user in users:
         ponto = Ponto.objects.filter(usuario=user).last()
 
@@ -22,36 +23,62 @@ def home(request):
             total_dia = Ponto.objects.total_day_time(
                 day=ponto.entrada.date(), user=user
             )
-
             if ponto.saida:
                 last_interaction = ponto.saida
             else:
                 last_interaction = ponto.entrada
 
-            pontos.append(
-                {
-                    "usuario": user,
-                    "last_interaction": last_interaction,
-                    "day": last_interaction.date(),
-                    "cliente": cliente,
-                    "tarefa": tarefa,
-                    "total_dia": total_dia,
-                    "tarefa_aberta": Ponto.objects.get_open_pontos(user),
-                }
+            ponto_status = Ponto.objects.get_open_pontos(user)
+            if ponto_status.count() > 0:
+                pontos_abertos.append(
+                    {
+                        "usuario": user,
+                        "last_interaction": last_interaction,
+                        "day": last_interaction.date(),
+                        "cliente": cliente,
+                        "tarefa": tarefa,
+                        "total_dia": total_dia,
+                        "tarefa_aberta": ponto_status,
+                    }
+                )
+            else:
+                pontos_fechados.append(
+                    {
+                        "usuario": user,
+                        "last_interaction": last_interaction,
+                        "day": last_interaction.date(),
+                        "cliente": cliente,
+                        "tarefa": tarefa,
+                        "total_dia": total_dia,
+                        "tarefa_aberta": ponto_status,
+                    }
+                )
+
+            # sort pontos by last_interaction desc and tarefa_aberta
+            pontos_abertos = sorted(
+                pontos_abertos, key=lambda k: k["last_interaction"], reverse=True
+            )
+            pontos_fechados = sorted(
+                pontos_fechados, key=lambda k: k["last_interaction"], reverse=True
             )
 
-            # sort pontos by last_interaction desc
-            pontos = sorted(pontos, key=lambda k: k["last_interaction"], reverse=True)
-
-    paginator = Paginator(pontos, 10)
+    paginator_pontos_abertos = Paginator(pontos_abertos, 5)
+    paginator_pontos_fechados = Paginator(pontos_fechados, 5)
 
     page = request.GET.get("page")
-    pontos = paginator.get_page(page)
+    
+    pontos_abertos = paginator_pontos_abertos.get_page(page)
+    pontos_fechados = paginator_pontos_fechados.get_page(page)
+
+    context = {
+        "pontos_abertos": pontos_abertos,
+        "pontos_fechados": pontos_fechados,
+    }
 
     # Carrega as datas iniciais e finais na sessão
     carrega_datas_session(request)
 
-    return render(request, "core/home.html", {"pontos": pontos})
+    return render(request, "core/home.html", context)
 
 
 def carrega_datas_session(request, user_id=None):
@@ -60,10 +87,14 @@ def carrega_datas_session(request, user_id=None):
 
     # Set data inicial
     first_day = now.replace(day=1).strftime("%Y-%m-%d")
+    # turn first_day into a datetime object
+    # first_day = datetime.strptime(first_day, "%Y-%m-%d")
     request.session["data_inicial"] = first_day
 
     # Set data final
     last_day = now.strftime("%Y-%m-%d")
+    # turn last_day into a datetime object
+    # last_day = datetime.strptime(last_day, "%Y-%m-%d")
     request.session["data_final"] = last_day
 
     if user_id is None:
