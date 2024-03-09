7c49ebc799c3cb06e92e452cc5caadaf92dd4084
Eduardo-Lucas <eduardolucas40@gmail.com>
2024-03-08 22:41:22 -0300

    Ferias admin

--- app/ferias/admin.py
+++ app/ferias/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
 from .models import Ferias
 
-admin.site.register(Ferias)
+
+class FeriasResource(resources.ModelResource):
+    """FeriasResource."""
+
+    class Meta:
+        """Meta."""
+
+        model = Ferias
+
+
+class FeriasAdmin(ImportExportModelAdmin):
+    """FeriasAdmin."""
+
+    resource_classes = [FeriasResource]
+
+
+admin.site.register(Ferias, FeriasAdmin)
