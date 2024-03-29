520abba50c49a7a8e748abb1da77d9e510cce821
Eduardo-Lucas <eduardolucas40@gmail.com>
2024-03-28 23:25:03 -0300

    Added UserProfile

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -10,6 +10,6 @@ python manage.py load_tipo_receita # Tipo Receita
 # python manage.py load_contato # Contatos not needed anymore
 # python manage.py load_feriado # Feriados
 # python manage.py load_clientes # Clientes
-# python manage.py createprofile # Profile
+python manage.py createprofile # Profile
 # python manage.py load_pontos_pandas # Pontos-2023
 gunicorn folha_ponto.wsgi:application --bind 0.0.0.0:10000
