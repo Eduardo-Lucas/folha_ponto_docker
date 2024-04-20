e98a9931760212c081d1d0a5c65c3a70bb582d71
Eduardo-Lucas <eduardolucas40@gmail.com>
2024-04-20 00:41:06 -0300

    Deploy sem rodar os management commands

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 python manage.py collectstatic --no-input
-python manage.py makemigrations
-python manage.py migrate
-python manage.py createdepartamento # Departamento
-python manage.py createuser # normal user
-python manage.py createsu # super user
-python manage.py load_tipo_receita # Tipo Receita
+# python manage.py makemigrations
+# python manage.py migrate
+# python manage.py createdepartamento # Departamento
+# python manage.py createuser # normal user
+# python manage.py createsu # super user
+# python manage.py load_tipo_receita # Tipo Receita
 # python manage.py load_contato # Contatos not needed anymore
 # python manage.py load_feriado # Feriados
 # python manage.py load_clientes # Clientes
