1ae9040d6dedfd62b04dcfb96027579ee0dd8b73
Eduardo-Lucas <eduardolucas40@gmail.com>
2024-05-01 11:00:27 -0300

    Volta configuracao original do DATABASES em settings.py

--- app/folha_ponto/settings.py
+++ app/folha_ponto/settings.py
@@ -116,23 +116,23 @@ WSGI_APPLICATION = "folha_ponto.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-# if DEBUG:
-#     DATABASES = {
-#         "default": {
-#             "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-#             "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
-#             "USER": os.environ.get("SQL_USER", "user"),
-#             "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
-#             "HOST": os.environ.get("SQL_HOST", "localhost"),
-#             "PORT": os.environ.get("SQL_PORT", "5432"),
-#         }
-#     }
-# else:
-DATABASES = {
-    "default": dj_database_url.parse(
-        os.environ.get("DATABASE_URL"), conn_max_age=600
-    ),
-}
+if DEBUG:
+    DATABASES = {
+        "default": {
+            "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+            "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
+            "USER": os.environ.get("SQL_USER", "user"),
+            "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+            "HOST": os.environ.get("SQL_HOST", "localhost"),
+            "PORT": os.environ.get("SQL_PORT", "5432"),
+        }
+    }
+else:
+    DATABASES = {
+        "default": dj_database_url.parse(
+            os.environ.get("DATABASE_URL"), conn_max_age=600
+        ),
+    }
 
 PASSWORD_HASHERS = [
     # Use the default password hasher
