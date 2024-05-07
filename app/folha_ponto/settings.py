1d0e52a6dfab90cc22dab8e093344265940873bd
lucasberenger <thegreatberenger@gmail.com>
2024-05-07 16:51:10 -0300

    add whitenoise settings to test

--- app/folha_ponto/settings.py
+++ app/folha_ponto/settings.py
@@ -189,14 +189,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR, "staticfiles")
+    os.path.join(BASE_DIR, "static")
 ]
 
 
-# WHITENOISE_AUTOREFRESH = True
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage" # Default value
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+WHITENOISE_AUTOREFRESH = True
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage" # Default value
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 
 MEDIA_URL = "/media/"
