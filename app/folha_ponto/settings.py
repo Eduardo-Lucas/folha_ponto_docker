cc7c9514c7d5c5d97c21dee6e7f2109ed7a66ea3
lucasberenger <thegreatberenger@gmail.com>
2024-05-07 18:16:02 -0300

    try to fix whitenoise issue

--- app/folha_ponto/settings.py
+++ app/folha_ponto/settings.py
@@ -173,16 +173,12 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 
-
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 MEDIA_URL = "/media/"
