5c44bbb89cc6e0028e92723a829881f399f4057f
lucasberenger <thegreatberenger@gmail.com>
2024-05-07 18:05:35 -0300

    try to fix whitenoise issue

--- app/folha_ponto/settings.py
+++ app/folha_ponto/settings.py
@@ -175,7 +175,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
+    os.path.join(BASE_DIR, "app/static")
 ]
 
 
