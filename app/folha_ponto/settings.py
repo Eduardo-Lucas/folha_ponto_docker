1af9241de268b4681814597c67e20206634009f3
lucasberenger <thegreatberenger@gmail.com>
2024-05-07 19:44:40 -0300

    try to fix the whitenoise issue

--- app/folha_ponto/settings.py
+++ app/folha_ponto/settings.py
@@ -179,13 +179,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 
-STORAGES = {
-    # ...
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
