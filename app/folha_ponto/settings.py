29911984c4586298421da3414f98e2c452d5cab9
lucasberenger <thegreatberenger@gmail.com>
2024-05-08 18:15:26 -0300

    try to fix the whitenoise issue

--- app/folha_ponto/settings.py
+++ app/folha_ponto/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from datetime import timedelta
 from pathlib import Path
+import whitenoise
 
 import dj_database_url
 from celery.schedules import crontab
