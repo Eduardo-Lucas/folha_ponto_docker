d06a2a09eda885937f3874c97597c7b1f162387e
Eduardo-Lucas <eduardolucas40@gmail.com>
2024-05-08 22:41:41 -0300

    Import - Export no app Refeição

--- app/refeicao/admin.py
+++ app/refeicao/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources
 
 from .models import Refeicao
 
-admin.site.register(Refeicao)
+
+class RefeicaoResource(resources.ModelResource):
+    """FeriasResource."""
+
+    class Meta:
+        """Meta."""
+
+        model = Refeicao
+
+
+class RefeicaoAdmin(ImportExportModelAdmin):
+    """FeriasAdmin."""
+
+    resource_classes = [RefeicaoResource]
+
+
+admin.site.register(Refeicao, RefeicaoAdmin)
