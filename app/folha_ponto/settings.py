5d26593be22560c265468f0d7a3c6a01e9e65990
lucasberenger <thegreatberenger@gmail.com>
2024-05-08 23:35:31 -0300

    try to fix whitenoise issues

--- app/folha_ponto/settings.py
+++ app/folha_ponto/settings.py
@@ -183,7 +183,7 @@ STATICFILES_DIRS = [
 STORAGES = {
     # ...
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
 
