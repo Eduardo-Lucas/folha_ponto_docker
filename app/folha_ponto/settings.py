23b548cc143d55013dd87b63c4fa30f39424b0fa
lucasberenger <thegreatberenger@gmail.com>
2024-05-08 23:07:02 -0300

    try to fix whitenoise issues

--- app/folha_ponto/settings.py
+++ app/folha_ponto/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from datetime import timedelta
 from pathlib import Path
-import whitenoise
 
 import dj_database_url
 from celery.schedules import crontab
@@ -43,6 +42,7 @@ ALLOWED_HOSTS = os.environ.get(
 
 INSTALLED_APPS = [
     "whitenoise.runserver_nostatic", #whitenoise
+    "django.contrib.staticfiles",
     "jazzmin",
     "corsheaders",
     "django.contrib.admin",
@@ -50,7 +50,6 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles",
 
     # apps
     "core",
@@ -175,16 +174,16 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles")
+    os.path.join(BASE_DIR, "static")
 ]
 
 STORAGES = {
     # ...
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
 
@@ -213,74 +212,74 @@ DATA_UPLOAD_MAX_MEMORY_SIZE = 1024 * 1024 * 20  # Example: 20MB
 # Increase maximum file size for file uploads
 FILE_UPLOAD_MAX_MEMORY_SIZE = 1024 * 1024 * 20  # Example: 20MB
 
-# JAZZMIN_SETTINGS = {
-#     "site_title": "your_site_name",
-#     "site_header": "your_site_header",
-#     "site_brand": "your_site_brand",
-#     "site_icon": "images/favicon.png",
-#     # Add your own branding here
-#     "site_logo": None,
-#     "welcome_sign": "Welcome to the your_site_name",
-#     # Copyright on the footer
-#     "copyright": "BV Contabilidade",
-#     "user_avatar": None,
-#     ############
-#     # Top Menu #
-#     ############
-#     # Links to put along the top menu
-#     "topmenu_links": [
-#         # Url that gets reversed (Permissions can be added)
-#         {
-#             "name": "voltar para BV Contabilidade",
-#             "url": "core:home",
-#             "permissions": ["auth.view_user"],
-#         },
-#         # model admin to link to (Permissions checked against model)
-#         # {"model": "auth.User"},
-#     ],
-#     #############
-#     # Side Menu #
-#     #############
-#     # Whether to display the side menu
-#     "show_sidebar": True,
-#     # Whether to aut expand the menu
-#     "navigation_expanded": True,
-#     # Custom icons for side menu apps/models See https://fontawesome.com/icons?d=gallery&m=free&v=5.0.0,5.0.1,5.0.10,5.0.11,5.0.12,5.0.13,5.0.2,5.0.3,5.0.4,5.0.5,5.0.6,5.0.7,5.0.8,5.0.9,5.1.0,5.1.1,5.2.0,5.3.0,5.3.1,5.4.0,5.4.1,5.4.2,5.13.0,5.12.0,5.11.2,5.11.1,5.10.0,5.9.0,5.8.2,5.8.1,5.7.2,5.7.1,5.7.0,5.6.3,5.5.0,5.4.2
-#     # for the full list of 5.13.0 free icon classes
-#     "icons": {
-#         "auth": "fas fa-users-cog",
-#         "auth.user": "fas fa-user",
-#         "users.User": "fas fa-user",
-#         "auth.Group": "fas fa-users",
-#         "admin.LogEntry": "fas fa-file",
-#     },
-#     # # Icons that are used when one is not manually specified
-#     "default_icon_parents": "fas fa-chevron-circle-right",
-#     "default_icon_children": "fas fa-arrow-circle-right",
-#     #################
-#     # Related Modal #
-#     #################
-#     # Use modals instead of popups
-#     "related_modal_active": False,
-#     #############
-#     # UI Tweaks #
-#     #############
-#     # Relative paths to custom CSS/JS scripts (must be present in static files)
-#     # Uncomment this line once you create the bootstrap-dark.css file
-#     # "custom_css": "css/bootstrap-dark.css",
-#     "custom_js": None,
-#     # Whether to show the UI customizer on the sidebar
-#     "show_ui_builder": False,
-#     ###############
-#     # Change view #
-#     ###############
-#     "changeform_format": "horizontal_tabs",
-#     # override change forms on a per modeladmin basis
-#     "changeform_format_overrides": {
-#         "auth.user": "collapsible",
-#         "auth.group": "vertical_tabs",
-#     },
-# }
+JAZZMIN_SETTINGS = {
+    "site_title": "your_site_name",
+    "site_header": "your_site_header",
+    "site_brand": "your_site_brand",
+    "site_icon": "images/favicon.png",
+    # Add your own branding here
+    "site_logo": None,
+    "welcome_sign": "Welcome to the your_site_name",
+    # Copyright on the footer
+    "copyright": "BV Contabilidade",
+    "user_avatar": None,
+    ############
+    # Top Menu #
+    ############
+    # Links to put along the top menu
+    "topmenu_links": [
+        # Url that gets reversed (Permissions can be added)
+        {
+            "name": "voltar para BV Contabilidade",
+            "url": "core:home",
+            "permissions": ["auth.view_user"],
+        },
+        # model admin to link to (Permissions checked against model)
+        # {"model": "auth.User"},
+    ],
+    #############
+    # Side Menu #
+    #############
+    # Whether to display the side menu
+    "show_sidebar": True,
+    # Whether to aut expand the menu
+    "navigation_expanded": True,
+    # Custom icons for side menu apps/models See https://fontawesome.com/icons?d=gallery&m=free&v=5.0.0,5.0.1,5.0.10,5.0.11,5.0.12,5.0.13,5.0.2,5.0.3,5.0.4,5.0.5,5.0.6,5.0.7,5.0.8,5.0.9,5.1.0,5.1.1,5.2.0,5.3.0,5.3.1,5.4.0,5.4.1,5.4.2,5.13.0,5.12.0,5.11.2,5.11.1,5.10.0,5.9.0,5.8.2,5.8.1,5.7.2,5.7.1,5.7.0,5.6.3,5.5.0,5.4.2
+    # for the full list of 5.13.0 free icon classes
+    "icons": {
+        "auth": "fas fa-users-cog",
+        "auth.user": "fas fa-user",
+        "users.User": "fas fa-user",
+        "auth.Group": "fas fa-users",
+        "admin.LogEntry": "fas fa-file",
+    },
+    # # Icons that are used when one is not manually specified
+    "default_icon_parents": "fas fa-chevron-circle-right",
+    "default_icon_children": "fas fa-arrow-circle-right",
+    #################
+    # Related Modal #
+    #################
+    # Use modals instead of popups
+    "related_modal_active": False,
+    #############
+    # UI Tweaks #
+    #############
+    # Relative paths to custom CSS/JS scripts (must be present in static files)
+    # Uncomment this line once you create the bootstrap-dark.css file
+    # "custom_css": "css/bootstrap-dark.css",
+    "custom_js": None,
+    # Whether to show the UI customizer on the sidebar
+    "show_ui_builder": False,
+    ###############
+    # Change view #
+    ###############
+    "changeform_format": "horizontal_tabs",
+    # override change forms on a per modeladmin basis
+    "changeform_format_overrides": {
+        "auth.user": "collapsible",
+        "auth.group": "vertical_tabs",
+    },
+}
 
 REDIS_URL = "redis://" + os.getenv("REDIS_HOST", "localhost") + ":6379/0"
 CELERY_BROKER_URL = REDIS_URL
