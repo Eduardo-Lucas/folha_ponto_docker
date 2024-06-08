6cfe71725580256b14d554a806a553ccccd770d4
Eduardo-Lucas <eduardolucas40@gmail.com>
2024-06-08 19:57:25 -0300

    run_seed.sh updated

--- run_seed.sh
+++ run_seed.sh
@@ -1,10 +1,23 @@
-docker-compose exec web-folha python manage.py collectstatic --no-input
-docker-compose exec web-folha python manage.py makemigrations
-docker-compose exec web-folha python manage.py migrate
-docker-compose exec web-folha python manage.py createuser # normal user
-docker-compose exec web-folha python manage.py createsu # super user
-docker-compose exec web-folha python manage.py load_tipo_receita # Tipo Receita
-docker-compose exec web-folha python manage.py load_contato # Contatos
-docker-compose exec web-folha python manage.py load_feriado # Feriados
-docker-compose exec web-folha python manage.py load_clientes # Clientes
-docker-compose exec web-folha python manage.py load_pontos_pandas # Pontos-2023
+# get initial time
+start=`date +%s`
+echo "Running seed script..."
+python manage.py makemigrations
+python manage.py migrate
+python manage.py flush
+python manage.py createsu # 0. eduardo as superuser
+python manage.py createdepartamento # 1. Departamento-current.csv
+python manage.py load_tipo_receita # 2. TipoReceita-current.csv
+python manage.py createuser # 3. normal user
+python manage.py createprofile # 4. UserProfile-current.csv
+python manage.py load_feriado # 5. Feriado-current.csv
+python manage.py load_ferias # 6. Ferias-current.csv
+python manage.py load_refeicao # 7. Refeicao-current.csv
+python manage.py load_clientes # 8. Cliente-current.csv
+python manage.py load_pontos # 9. Ponto-current.csv
+# get final time
+end=`date +%s`
+# calculate execution time
+runtime=$((end-start))
+# translate seconds to minutes
+minutes=$((runtime/60))
+echo "Execution time: $minutes minutes"
