23a37de2587b65fb92e6a6a65513c8f05f3f6c17
Eduardo-Lucas <eduardolucas40@gmail.com>
2024-06-16 19:20:11 -0300

    load_pontos with counter

--- app/apontamento/management/commands/load_pontos.py
+++ app/apontamento/management/commands/load_pontos.py
@@ -1,11 +1,9 @@
-import csv
-from datetime import datetime, time, timedelta
 
-import pandas as pd
-import pytz
+import csv
+from datetime import datetime
+
 from apontamento.models import Ponto, TipoReceita
 from cliente.models import Cliente
-from django.conf import settings
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
 
@@ -16,55 +14,71 @@ class Command(BaseCommand):
     help = "Loads Ponto."
 
     def handle(self, *args, **options):
-        load_data_from_csv()
+        populate_pontos_from_csv()
 
 
-def load_data_from_csv():
-    """Load data from csv file."""
-    df = pd.read_csv("Ponto-current.csv")
-    # df = pd.read_csv("Ponto-current.csv")
+def populate_pontos_from_csv():
+    """Populate Ponto from a CSV file."""
+    with open("Ponto-current.csv", "r", encoding="utf-8") as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        # next(csv_reader)  # Skip the header row
+        contador = 0
+        print("Processing Ponto upload...")
+        for row in csv_reader:
+            initial_time = datetime.now()
 
-    # pontos = []
-    for index, row in df.iterrows():
-        cliente = (
-            Cliente.objects.get(id=row["cliente_id"])
-            if row["cliente_id"] and str(row["cliente_id"]).isdigit()
-            else None
-        )
+            # only prints if contador is multiple of 1000
+            if contador % 1000 == 0:
+                print(f"Processing contador {contador}...")
 
-        # Split the line into multiple lines for better readability
-        # tipo_receita = (
-        #     TipoReceita.objects.get(id=pd.to_numeric(row['tiporeceita_id']))
-        #     if row['tiporeceita_id']
-        #     else None
-        # )
-        tipo_receita = None
+            if row["saida"]:
+                saida_fmt = row["saida"]
+            else:
+                saida_fmt = None
 
-        if pd.notnull(row["saida"]):
-            saida = pd.to_datetime(
-                str(row["saida"]).split(".", maxsplit=1)[0], format="%Y-%m-%d %H:%M:%S"
+            usuario_var = User.objects.get(id=row["usuario"])
+
+            if row["cliente_id"]:
+                cliente_var = Cliente.objects.get(id=row["cliente_id"])
+            else:
+                cliente_var = None
+
+            tipo_receita_var = TipoReceita.objects.get(id=row["tipo_receita"])
+
+            ponto_id = int(row["id"])
+            entrada = row["entrada"]
+            primeiro = row["primeiro"]
+            segundo = row["segundo"]
+            atraso = row["atraso"]
+            saida = saida_fmt
+            usuario = usuario_var
+            fechado = row["fechado"]
+            cliente_id = cliente_var
+            tipo_receita = tipo_receita_var
+            atrasoautorizado = row["atrasoautorizado"]
+            over_10_hours_authorization = row["over_10_hours_authorization"]
+
+            # Create a new Ponto object and save it to the database
+            ponto = Ponto(
+                id=ponto_id,
+                entrada=entrada,
+                primeiro=primeiro,
+                segundo=segundo,
+                atraso=atraso,
+                saida=saida,
+                usuario=usuario,
+                fechado=fechado,
+                cliente_id=cliente_id,
+                tipo_receita=tipo_receita,
+                atrasoautorizado=atrasoautorizado,
+                over_10_hours_authorization=over_10_hours_authorization,
             )
-        else:
-            saida = None
+            ponto.save()
+            contador += 1
 
-        entrada = pd.to_datetime(
-            str(row["entrada"]).split(".", maxsplit=1)[0], format="%Y-%m-%d %H:%M:%S"
-        )
-        ponto = Ponto(
-            id=row["id"],
-            entrada=entrada,
-            primeiro=row["primeiro"],
-            segundo=row["segundo"],
-            atraso=row["atraso"],
-            saida=saida,
-            usuario=(User.objects.get(id=row["usuario"]) if row["usuario"] else None),
-            fechado=row["fechado"],
-            cliente_id=cliente,
-            tipo_receita=tipo_receita,
-            atrasoautorizado=row["atrasoautorizado"],
-        )
-        ponto.save()
-        # pontos.append(ponto)
-
-    # Ponto.objects.bulk_create(pontos)
-    print("Pontos have been successfully uploaded using pandas.")
+    final_time = datetime.now()
+    time_diff = final_time - initial_time
+    # time_diff formatted in hours, minutes and seconds
+    total_seconds = time_diff.total_seconds()
+    print(f"Processing time: {total_seconds}")
+    print(f"{contador} Pontos have been successfully uploaded.")
