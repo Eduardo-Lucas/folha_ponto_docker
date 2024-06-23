2e3fd2df6a76aa6aaf3242794593a42dbb2767b4
Eduardo-Lucas <eduardolucas40@gmail.com>
2024-06-23 12:11:53 -0300

    entrypoint.sh replaces build.sh

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -11,4 +11,18 @@
 # python manage.py load_clientes # Cliente-2024-05-10.csv
 # python manage.py createprofile # UserProfile-2024-05-10.csv
 # python manage.py load_pontos_pandas # Ponto-2024-04-20.csv
-gunicorn folha_ponto.wsgi:application --bind 0.0.0.0:10000
+# gunicorn folha_ponto.wsgi:application --bind 0.0.0.0:10000
+
+# brought build.sh commands here
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+# Modify this line as needed for your package manager (pip, poetry, etc.)
+# pip install -r requirements.txt
+
+# Convert static asset files
+python manage.py collectstatic --no-input
+
+# Apply any outstanding database migrations
+python manage.py migrate
