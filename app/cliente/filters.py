564d636036c8271ab1c7154cee9600551e371d0f
Eduardo-Lucas <eduardolucas40@gmail.com>
2024-10-10 18:43:19 -0300

    Customizado o campo codigosistema para excluir os zeros da esquerda e fazer a pesquisa.

--- app/cliente/filters.py
+++ app/cliente/filters.py
@@ -3,12 +3,18 @@ import django_filters
 from .models import Cliente
 
 class ClienteFilter(django_filters.FilterSet):
+    def filter_codigosistema(self, queryset, name, value):
+        # Strip leading zeros from user input
+        value = value.lstrip("0")
+        # Filter queryset based on modified value
+        return queryset.filter(**{name: value})
+
     class Meta:
         model = Cliente
 
         fields = {
             "nomerazao": ["icontains"],
-            "codigosistema": ["icontains"],
+            # "codigosistema": ["icontains"],
             "documento": ["icontains"],
 
         }
@@ -16,5 +22,5 @@ class ClienteFilter(django_filters.FilterSet):
     def __init__(self, *args, **kwargs):
         super(ClienteFilter, self).__init__(*args, **kwargs)
         self.filters["nomerazao__icontains"].label = "Razão Social"
-        self.filters["codigosistema__icontains"].label = "Código"
-        self.filters["documento__icontains"].label = "CPF ou CNPJ"
+        self.filters["documento__icontains"].label = "CPF/CNPJ"
+        self.filters["custom_codigosistema"] = django_filters.CharFilter(field_name="codigosistema", method=self.filter_codigosistema, label="Código")
