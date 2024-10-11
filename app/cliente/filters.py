13f6e0997a3ed2581220b6454b07ebf18d8c0adc
Eduardo-Lucas <eduardolucas40@gmail.com>
2024-10-11 08:38:24 -0300

    Ajustes finais na tela de filtro de clientes

--- app/cliente/filters.py
+++ app/cliente/filters.py
@@ -3,6 +3,12 @@ import django_filters
 from .models import Cliente
 
 class ClienteFilter(django_filters.FilterSet):
+
+    custom_codigosistema = django_filters.CharFilter(field_name="codigosistema", method="filter_codigosistema", label="Código")
+    nomerazao = django_filters.CharFilter(field_name="nomerazao", lookup_expr="icontains", label="Razão Social")
+    documento = django_filters.CharFilter(field_name="documento", lookup_expr="icontains", label="CPF/CNPJ")
+
+
     def filter_codigosistema(self, queryset, name, value):
         # Strip leading zeros from user input
         value = value.lstrip("0")
@@ -12,15 +18,11 @@ class ClienteFilter(django_filters.FilterSet):
     class Meta:
         model = Cliente
 
-        fields = {
-            "nomerazao": ["icontains"],
-            # "codigosistema": ["icontains"],
-            "documento": ["icontains"],
+        fields = ['custom_codigosistema', 'nomerazao', 'documento']
 
-        }
 
     def __init__(self, *args, **kwargs):
         super(ClienteFilter, self).__init__(*args, **kwargs)
-        self.filters["nomerazao__icontains"].label = "Razão Social"
-        self.filters["documento__icontains"].label = "CPF/CNPJ"
         self.filters["custom_codigosistema"] = django_filters.CharFilter(field_name="codigosistema", method=self.filter_codigosistema, label="Código")
+        self.filters["nomerazao"].label = "Razão Social"
+        self.filters["documento"].label = "CPF/CNPJ"
